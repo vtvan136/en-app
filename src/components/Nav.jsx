40e85221ef74f7def041cd5c95f31ac696cf79f3
vtvan136 <vtvan136@gmail.com>
2023-05-24 19:05:11 +0700

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,42 +1,42 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <>
+
       <nav className="relative">
         <ul className="flex justify-around py-2 bottom-0 fixed bg-black w-full">
           <li className="basis-1/5">
-            <a href="/" className="flex-col flex justify-center items-center text-white gap-2 hover:bg-white hover:text-black p-2 rounded-2xl">
+            <Link to="/" className="flex-col flex justify-center items-center text-white gap-2 hover:bg-white hover:text-black p-2 rounded-2xl">
               <i className=" bx bx-home-alt"></i>
               Home
-            </a>
+            </Link>
           </li>
           <li className="basis-1/5">
            
-            <a href="/topic" className="flex-col flex justify-center items-center text-white gap-2 hover:bg-white hover:text-black p-2 rounded-2xl">
+            <Link to="/topic" className="flex-col flex justify-center items-center text-white gap-2 hover:bg-white hover:text-black p-2 rounded-2xl">
                <i className=" bx bx-collection"></i>Topics
-            </a>
+            </Link>
           </li>
           <li className="basis-1/5">
             
-              <a
-                href="/"
+              <Link        to="/"
                 className="flex-col flex justify-center items-center text-white gap-2 hover:bg-white hover:text-black p-2 rounded-2xl"
               >
                 <i className=" bx bx-bar-chart-alt-2"></i>Rank
-              </a>
+              </Link>
             
           </li>
           <li className="basis-1/5">
           
-            <a href="/" className="flex-col flex justify-center items-center text-white gap-2 hover:bg-white hover:text-black p-2 rounded-2xl">
+            <Link to="/" className="flex-col flex justify-center items-center text-white gap-2 hover:bg-white hover:text-black p-2 rounded-2xl">
                <i className=" bx bx-library"></i>Dictionary
-            </a>
+            </Link>
           </li>
           <li className="basis-1/5">
-            <a href="/" className="flex-col flex justify-center items-center text-white gap-2 hover:bg-white hover:text-black p-2 rounded-2xl">
+            <Link to="/" className="flex-col flex justify-center items-center text-white gap-2 hover:bg-white hover:text-black p-2 rounded-2xl">
               <i className=" bx bx-user"></i>Account
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
