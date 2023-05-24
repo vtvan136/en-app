fb4bc7cd0eea06d2616888126c21e0e9bca86ffd
vtvan136 <vtvan136@gmail.com>
2023-05-24 18:52:01 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from './pages/Home'
 import Nav from './components/Nav'
 import { Routes, Route } from "react-router-dom";
 import Topics from './pages/Topics';
+import Page404 from './pages/Page404';
 const App = () => {
   return (
     <>
       <Nav/>
       <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<Page404 />} />
       <Route path="/topic" element={<Topics />} />
     </Routes>
     </>
