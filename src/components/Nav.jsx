5a269cf3228f87d81b71123fa3f536decc4e0cc0
vtvan136 <vtvan136@gmail.com>
2023-05-24 18:34:23 +0700

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,19 +5,19 @@ const Nav = () => {
     <>
       <nav className="relative">
         <ul className="flex justify-around py-2 bottom-0 fixed bg-black w-full">
-          <li>
+          <li className="basis-1/5">
             <a href="/" className="flex-col flex justify-center items-center text-white gap-2 hover:bg-white hover:text-black p-2 rounded-2xl">
               <i className=" bx bx-home-alt"></i>
               Home
             </a>
           </li>
-          <li>
+          <li className="basis-1/5">
            
             <a href="/" className="flex-col flex justify-center items-center text-white gap-2 hover:bg-white hover:text-black p-2 rounded-2xl">
                <i className=" bx bx-collection"></i>Topics
             </a>
           </li>
-          <li>
+          <li className="basis-1/5">
             
               <a
                 href="/"
@@ -27,13 +27,13 @@ const Nav = () => {
               </a>
             
           </li>
-          <li>
+          <li className="basis-1/5">
           
             <a href="/" className="flex-col flex justify-center items-center text-white gap-2 hover:bg-white hover:text-black p-2 rounded-2xl">
                <i className=" bx bx-library"></i>Dictionary
             </a>
           </li>
-          <li>
+          <li className="basis-1/5">
             <a href="/" className="flex-col flex justify-center items-center text-white gap-2 hover:bg-white hover:text-black p-2 rounded-2xl">
               <i className=" bx bx-user"></i>Account
             </a>
